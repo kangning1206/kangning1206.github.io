1feb4a8895c83ab2895a3cfae1f39cb4567514d8
kangning1206 <kangning1206@163.com>
2018-12-01 11:17:35 +0800

    Site updated: 2018-12-01 11:17:34

--- js/index.js
+++ js/index.js
@@ -1,86 +1,58 @@
-(function () {
-    'use strict';
+var $body = document.body
+var $toggle = document.querySelector('.navbar-toggle')
+var $navbar = document.querySelector('#bosenyblog-navbar')
+var $collapse = document.querySelector('.navbar-collapse')
+var topBtn = document.querySelector('.gotop-btn')
+var $toc = document.querySelector('#toc')
 
-    function Sky(config) {
+var win = $(window) //得到窗口对象
+var sc = $(document) //得到document文档对象。
+var headerHeight = $('.header-container').height()
 
-    }
-
-    Sky.prototype.init = function () {
-        this.tocFixed();
-        this.tocActive();
-        this.backToTop();
-        this.mobileNavToggle();
-    };
-
-    // make toc stay in the visible area
-    Sky.prototype.tocFixed = function () {
-        var HEADER_OFFSET = 20;
-        var $toc = $('#post-toc');
-        if ($toc.length) {
-            var minScrollTop = $toc.offset().top;
-            $(window).scroll(function () {
-                var scrollTop = $(window).scrollTop();
-                if (scrollTop < minScrollTop) {
-                    $toc.css({'position': 'absolute', 'top': minScrollTop - 70});
-                } else {
-                    $toc.css({'position': 'fixed', 'top': HEADER_OFFSET + 'px'});
-                }
-            });
-        }
-    };
-
-    // current toc follows the content when scrolling
-    Sky.prototype.tocActive = function () {
-        var HEADER_OFFSET = 30;
-        var $toclink = $('.toc-link');
-        var $headerlink = $('.headerlink');
-
-        var headerlinkTop = $.map($headerlink, function (link) {
-            return $(link).offset().top;
-        });
-        $(window).scroll(function () {
-            var scrollTop = $(window).scrollTop();
-            for (var i = 0; i < $toclink.length; i++) {
-                var currentHeaderTop = headerlinkTop[i] - HEADER_OFFSET,
-                    nextHeaderTop = i + 1 === $toclink.length ? Infinity : headerlinkTop[i + 1] - HEADER_OFFSET;
-
-                if (currentHeaderTop < scrollTop && scrollTop <= nextHeaderTop) {
-                    $($toclink[i]).addClass('active');
-                } else {
-                    $($toclink[i]).removeClass('active');
-                }
-            }
-        });
-    };
-
-    // back to top
-    Sky.prototype.backToTop = function () {
-        var $backToTop = $('#back-to-top');
-
-        $backToTop.click(function () {
-            console.log('click');
-            $('html,body').animate({ scrollTop: 0 });
-        });
-    };
-
-    // mobile nav toggle
-    Sky.prototype.mobileNavToggle = function () {
-        var $mobileNav = $('.mobile-nav-icon'),
-            $mobileMenu = $('.mobile-menu');
-
-        $mobileNav.click(function () {
-            if (!$mobileMenu.hasClass('show-menu')) {
-                $mobileMenu.addClass('show-menu');
-                $mobileMenu.removeClass('hide-menu');
-                $mobileNav.addClass('show-menu').removeClass('hide-menu');
-            } else {
-                $mobileMenu.addClass('hide-menu');
-                $mobileMenu.removeClass('show-menu');
-                $mobileNav.removeClass('show-menu').addClass('hide-menu');
-            }
-        })
-    };
-
-    var sky = new Sky();
-    sky.init();
-})(window);
+$toggle.addEventListener('click', handleMagic)
+function handleMagic(e) {
+  if ($navbar.className.indexOf('in') > 0) {
+    // CLOSE
+    $navbar.className = ''
+    // wait until animation end.
+    setTimeout(function() {
+      // prevent frequently toggle
+      if ($navbar.className.indexOf('in') < 0) {
+        $collapse.style.height = '0px'
+      }
+    }, 400)
+  } else {
+    // OPEN
+    $collapse.style.height = 'auto'
+    $navbar.className += ' in'
+  }
+}
+$(window).scroll(function() {
+  $(this).scrollTop() > 400
+    ? $('.gotop-btn').css('display', 'block')
+    : $('.gotop-btn').hide()
+  var $catalog = $('#toc')
+  // $catalog.show()
+  if ($(this).scrollTop() > headerHeight + 95) {
+    $catalog.addClass('fixed')
+    // $catalog.removeAttr("left")
+  } else {
+    $catalog.removeClass('fixed')
+    // $catalog.css("left", "15%")
+  }
+})
+$('.gotop-btn').hover(
+  function() {
+    $(this).addClass('top-active')
+  },
+  function() {
+    $(this).removeClass('top-active')
+  }
+)
+topBtn.addEventListener('click', () => {
+  $('html,body').animate({ scrollTop: '0px' }, 500)
+  $('.gotop-btn').removeClass('top-active')
+})
+if ($toc && $toc.childElementCount && $toc.childElementCount <= 1) {
+  $toc.style.display = 'none'
+}
